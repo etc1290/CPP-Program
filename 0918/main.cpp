be2dca2566e424475cf51a232aa70d33a9183f6a
etc1290 <52283388+etc1290@users.noreply.github.com>
2020-09-18 10:57:20 +0800

    0918

---
+++ 0918/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	string x;
+	cin >> x;
+	cout<< x <<endl;
+	cout << "國立聯合大學\n";
+	cout << 1+3+5+7+9+11+13 << endl;
+	cout << 81*52-12 << endl;
+	cout << 53*53*3.14159657 << endl;
+	cout << 2*57*3.14159657 << endl;
+	cout << 100*99-98+97%25/3 << endl;
+
+	
+	return 0;
+}
