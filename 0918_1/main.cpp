4049e49161a075e5e0ec8b468509c065c4bf16a0
etc1290 <52283388+etc1290@users.noreply.github.com>
2020-09-18 11:06:12 +0800

    0918_Setup

--- 0918_1/main.cpp
+++ 0918_1/main.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
-#include <string>
-
-using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main_1(int argc, char** argv) {
-
-	cout << "Hello!";
-
+int main(int argc, char** argv) {
 	return 0;
 }
