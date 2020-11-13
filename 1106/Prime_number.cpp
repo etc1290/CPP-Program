0cabb1b9b715a51edc573d556f956875ca01e9c3
etc1290 <52283388+etc1290@users.noreply.github.com>
2020-11-13 09:56:00 +0800

    Update Prime_number.cpp

--- 1106/Prime_number.cpp
+++ 1106/Prime_number.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+int Pcount =0 , NPcount = 0;
+
 bool Prime(int n){
     if(n==1) return 0;
     for(int i=2; i*i<=n; i++)
@@ -10,15 +12,11 @@ bool Prime(int n){
     }
     return 1;
 }
- 
 
-int main(int argc, char** argv) {
-	int n, Pcount =0 , NPcount = 0;
-	cin >> n;
-	
-	for(int i = 1 ; i < n +1; i++ ) {
+int Prime_range(int begin, int end, bool show = false){
+	for(int i = begin ; i < end +1; i++ ) {
 		if(Prime(i)){
-			cout << i << " Is a Prime" << endl;
+			if(show) cout << i << " Is a Prime" << endl;
 			Pcount++;	
 		} 
 		else {
@@ -26,8 +24,30 @@ int main(int argc, char** argv) {
 			NPcount++;
 		}
 	} 
+}
+ 
+
+int main(int argc, char** argv) {
+	int n1, n2; 
+	char show_input;
+	bool show;
 	
-	cout << "There have " << Pcount << " Prime in " << n << endl;
-	cout << "There have " << NPcount << " Non-prime in " << n << endl;	
+	//Data Input
+	cin >> n1;
+	cin >> n2;
+	cout << "Show Prime Number ? (y/n)" << endl;
+	cin >> show_input;
+	
+	//Show All of the Prime number
+	if(show_input == 'y' || show_input == 'Y') show = true;
+	else show = false;
+	
+	//to Prime function
+	Prime_range(n1, n2, show);
+	
+	
+	
+	cout << "There have " << Pcount << " Prime in " << n1  << " to " << n2 << endl;
+	cout << "There have " << NPcount << " Non-prime in " << n1  << " to " << n2 << endl;
 	return 0;
 }
